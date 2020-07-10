10cd0bedcc1cce9282c28cb8dd46892ed14b434b
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2020-07-10 23:50:13 +0200

    let the property names be same as the variables used

--- src/main/java/com/epo/webdriver/manager/Properties.java
+++ src/main/java/com/epo/webdriver/manager/Properties.java
@@ -6,19 +6,19 @@ public class Properties {
     private static String browserName;
 
     public static String getHostName() {
-        //  Set 'host' property from CI (or command line parameter) and get the property here.
+        //  Set 'hostName' property from CI (or command line parameter) and get the property here.
         // If you want to test with a default value for this property, give it here, say "local".
-        hostName = System.getProperty("host","local");
-        System.out.println("System property key:'host'; value: " + hostName);
+        hostName = System.getProperty("hostName","local");
+        System.out.println("System property key:'hostName'; value: " + hostName);
 
         return hostName;
     }
 
     public static String getBrowserName() {
-        //  Set 'browser' property from CI (or command line parameter) and get the property here.
+        //  Set 'browserName' property from CI (or command line parameter) and get the property here.
         // If you want to test with a default value for this property, give it here, say "chrome".
-        browserName = System.getProperty("browser","chrome");
-        System.out.println("System property key:'browser'; value: " + browserName);
+        browserName = System.getProperty("browserName","chrome");
+        System.out.println("System property key:'browserName'; value: " + browserName);
 
         return browserName;
     }
