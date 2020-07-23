482e440998f71d9ad4cd309ecb85f900e6c8aeea
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2020-07-23 15:40:36 +0200

    Refactored ps1 script to show all options related steps one by one

--- Set-SeleniumGrid.ps1
+++ Set-SeleniumGrid.ps1
@@ -2,7 +2,16 @@
     #  . .\Set-SeleniumGrid.ps1
     # Start-ComposeGridAndTests
     
-# Set selenium grid using docker swarm
+# Option 02: This will run the container and next step will run the tests on container
+function Start-TestContainerOnly{
+    docker-compose -f .\docker-compose-test.yml up
+}
+function Start-TestsOnTestContainerItself{
+    # At this moment, I have only installed chrome driver for local execution
+    docker container exec -it grid_test mvn clean test -Dhost=container -Dbrowser=chrome
+}
+
+# Option 03: Set selenium grid using docker swarm (and then you can run tests from local)
 function Set-SwarmGrid {
     [CmdletBinding()]
 	param(
@@ -16,19 +25,8 @@ function Set-SwarmGrid {
     docker service ls
 }
 
-function Remove-SwarmGrid {
-    [CmdletBinding()]
-	param(
-            [String]$Path 
-        )
-    
-    # remove  grid
-    docker stack ls
-    docker stack rm grid
-    docker stack ls
-}
-
-function Set-SwarmGridAndTests {
+# Option 04: Set selenium grid using docker swarm Or compose and run tests from test container on grid 
+function Set-GridAndTestContainersViaDockerSwarm {
     [CmdletBinding()]
 	param(
         )
@@ -39,16 +37,37 @@ function Set-SwarmGridAndTests {
     docker stack deploy -c stack.yml grid
     docker stack ls
     docker service ls
-    docker service logs --follow grid_test-chrome
+    docker service logs --follow grid_test
 }
 
-function Start-ComposeGridAndTests{
+function Set-GridAndTestContainersViaDockerCompose{
     [CmdletBinding()]
 	param(
         )
     
     # override docker compose grid with test details.
     docker-compose -f ./docker-compose-grid.yml -f ./docker-compose-test.yml -f ./docker-compose-test-depend-on-grid.yml up
+}
+
+# This will run tests on grid (no matter if they were started from swarm or from compose)
+function Start-TestsOnGridFromContainer{
+    $testContainerId = docker container ls -f name=grid_test -q
+    docker container exec -it $testContainerId mvn clean test -Dhost=grid -DaccessGridFrom=container -Dbrowser=firefox
+
+    # Grid can be accessed here: http://localhost:4444/grid/console
+}
+
+# House keeping functions
+function Remove-SwarmGrid {
+    [CmdletBinding()]
+	param(
+            [String]$Path 
+        )
+    
+    # remove  grid
+    docker stack ls
+    docker stack rm grid
+    docker stack ls
 }
 
 function Remove-AllDockerContainers {
